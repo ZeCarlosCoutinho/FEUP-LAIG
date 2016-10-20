05ec3bf94afb9f1ccca119732268fac610bfb308
filpez <joaofilipepereiracosta@hotmail.com>
2016-10-20 13:44:31 +0100

    Corrigi Torus

--- reader/primitives/MyTorus.js
+++ reader/primitives/MyTorus.js
@@ -2,16 +2,13 @@
  * MyTorus
  * @constructor
  */
-function MyTorus(scene, inner, outer, slices, loops) {
+function MyTorus(scene, minorRadius, majorRadius, slices, loops) {
  	CGFobject.call(this, scene);
 		
-	this.inner = inner;
-	this.outer = outer;
+	this.majorRadius = majorRadius;
+	this.minorRadius = minorRadius;
 	this.slices = slices;
 	this.loops = loops;
-
-	this.baseRadius = (inner + outer) / 2;
-	this.deltaRadius = (outer - inner) / 2;
 	
 	this.alpha = 2 * Math.PI / slices;
 	this.beta = 2 * Math.PI / loops;
@@ -29,9 +26,9 @@ MyTorus.prototype.initBuffers = function() {
  	for (var i = 0; i <= this.loops; i++) {
  		for (var j = 0; j <= this.slices; j++) {
  			this.vertices.push(
- 				Math.cos(this.beta * i) * (this.baseRadius + Math.cos(this.alpha * j) * this.deltaRadius),
- 				Math.sin(this.beta * i) * (this.baseRadius + Math.cos(this.alpha * j) * this.deltaRadius),
- 				Math.sin(this.alpha * j) * this.deltaRadius);
+ 				Math.cos(this.beta * i) * (this.majorRadius + Math.cos(this.alpha * j) * this.minorRadius),
+ 				Math.sin(this.beta * i) * (this.majorRadius + Math.cos(this.alpha * j) * this.minorRadius),
+ 				Math.sin(this.alpha * j) * this.minorRadius);
 			this.normals.push(
 				Math.cos(this.beta * i) * Math.cos(this.alpha * j),
 				Math.sin(this.beta * i) * Math.cos(this.alpha * j),
