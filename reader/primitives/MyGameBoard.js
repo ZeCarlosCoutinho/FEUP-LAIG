2a913f93bdfc4978ce2d9536c2d00d29817b798e
filpez <joaofilipepereiracosta@hotmail.com>
2016-12-07 18:09:22 +0000

    Small Effect Added

--- reader/primitives/MyGameBoard.js
+++ reader/primitives/MyGameBoard.js
@@ -39,6 +39,12 @@ MyGameBoard.prototype.displaySelected = function () {
 			this.scene.translate(0,0.001,0); //TODO This may not be the best way, cause we are displaying 2 times;
 			this.scene.translate((i-1)/this.dX,0, (j-1)/this.dY);
 			this.scene.scale(1/this.dX, 1/((this.dY+this.dX)/2), 1/this.dY);
+			
+			//TODO Opcional effect
+			this.scene.translate(0.45,0,0.45);
+			this.scene.scale(0.8,1,0.8);
+			this.scene.translate(-0.5,0,-0.5);
+
 			this.tiles[i][j].display();
 		this.scene.popMatrix();
 	}
