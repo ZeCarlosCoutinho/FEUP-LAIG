142d13ec743a64f7579c6053791c960532cd0506
filpez <joaofilipepereiracosta@hotmail.com>
2016-12-20 12:01:36 +0000

    Corrigi uns bugs que tinhas, no meu ta a funcionar tudo direito

--- reader/game/states/GameState.js
+++ reader/game/states/GameState.js
@@ -16,19 +16,21 @@ GameState.prototype.constructor = GameState;
 GameState.prototype.remakeMove = function()
 {
 	var movesDone = this.board.movesDone;
-	var lastmove = movesDone.pop();
-	if(lastmove.player == "pc")
-	{
-		movesDone.pop();
-	}
-
-	//Board Reset
-	this.board.initializePieces();
-	this.board.movesDone = [];
-
-	for(var i = 0; i < movesDone.length; i++)
-	{
-		var move = new MoveSelected(movesDone[i].player, this.board, movesDone[i].move);
-		move.next();
+	if (movesDone.length > 0){
+		var lastmove = movesDone.pop();
+		if(lastmove.player.type == "pc")
+		{
+			movesDone.pop();
+		}
+	
+		//Board Reset
+		this.board.initializePieces();
+		this.board.movesDone = [];
+	
+		for(var i = 0; i < movesDone.length; i++)
+		{
+			var move = new MoveSelected(movesDone[i].player, this.board, movesDone[i].move);
+			move.next();
+		}
 	}
 }
