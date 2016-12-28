76709d389cf6cb45beb6a6823cc845061d031064
filpez <joaofilipepereiracosta@hotmail.com>
2016-12-28 11:13:16 +0000

    Revert "Keyframes still no working. READ the notes below:"
    This reverts commit 63e44ff1b084a12528675e6905fc9be5cfa19902.

--- reader/game/states/MoveSelected.js
+++ reader/game/states/MoveSelected.js
@@ -39,13 +39,7 @@ MoveSelected.prototype.display = function()
 						var x = mov.coordinates[0];
 						var z = mov.coordinates[1]
 						if (pieces[x][z] && pieces[i][j] == pieces[x][z])
-						{
 							this.scene.multMatrix(this.animationList[m].matrix);
-							if(this.outOfBoardAnimationList[m] && this.animationList[0].finished)
-							{
-								this.scene.multMatrix(this.outOfBoardAnimationList[m].matrix);
-							}
-						}
 					}
 					this.scene.translate((i-1)/9, 0, (j-1)/9);
 					this.scene.scale(1/9, 1/9, 1/9);
@@ -55,9 +49,7 @@ MoveSelected.prototype.display = function()
 		}
 	}
 
-   	/*if(!this.animation_on || this.animationList[0].finished) //TODO change
-		this.next();*/
-	if(!this.animation_on || (this.animationList[0].finished && this.checkOutAnimationFinished())) //TODO change
+   	if(!this.animation_on || this.animationList[0].finished)
 		this.next();
 };
 
@@ -72,38 +64,10 @@ MoveSelected.prototype.next = function(){
 
 MoveSelected.prototype.calculateAnimation = function(){
 	this.animationList = [];
-	this.outOfBoardAnimationList = [];
 	var finalTime = this.moveList[0].distance;
 	for (var k in this.moveList){
 		this.animationList.push(new PieceAnimation(this.moveList[k].difference, finalTime - this.moveList[k].distance, this.moveList[k].distance, this.scene.animationSpeed));
-		if(this.moveList[k].isGoingOutOfBoard())
-		{
-			//Push to a list to do the animation later
-			var height = 0.2;
-			var speed = 0.1;
-			var point1 = vec3.fromValues(this.moveList[k].destination[0]/9, 0, this.moveList[k].destination[1]/9);
-			var point2 = vec3.fromValues(this.moveList[k].destination[0]/9, height, this.moveList[k].destination[1]/9);
-
-			//TODO I need the Move to have reference to the player, owner of that piece.
-			//TODO Sum point3/point4 to the coordinates of the auxiliarBoard origin.
-			var point3 = this.board.auxiliarBoard.lastAvailableSlotPosition("Red"); //This assumes always its the red player
-			var point4 = point3;
-			point3[1] += height;
-			
-			var keyframeAnimation = new DisappearingAnimation([point1, point2, point3, point4], speed);
-			this.outOfBoardAnimationList.push(keyframeAnimation);
-		}
-		else //Just to keep the paralellism with the moveList
-		{
-			this.outOfBoardAnimationList.push(undefined);
-		}
-		/*TEST
-		var pointsList = [
-		vec3.fromValues(0, 0, 0), 
-		vec3.fromValues(0.5, 0, 0), 
-		vec3.fromValues(0, 0.5, 0),
-		vec3.fromValues(0,0,0.5)
-		];
+		/*var pointsList = [vec3.fromValues(0, 0, 0), vec3.fromValues(1, 0, 0), vec3.fromValues(0, 0, 1)];
 		this.animationList.push(new DisappearingAnimation(pointsList, 0.1));*/
 	}
 }
@@ -111,23 +75,4 @@ MoveSelected.prototype.calculateAnimation = function(){
 MoveSelected.prototype.updateAnimation = function (currTime){
 	for(var a of this.animationList)
 		a.updateMatrix(currTime);
-	if(this.animationList[0].finished)
-	{
-		for(var b of this.outOfBoardAnimationList)
-		{
-			if(b)
-				b.updateMatrix(currTime);
-		}
-	}
-}
-
-MoveSelected.prototype.checkOutAnimationFinished = function()
-{
-	for(var a of this.outOfBoardAnimationList)
-	{
-		if(a && !(a.finished)) //If one of the animations isn't finished
-			return false;
-	}
-
-	return true;
 }
